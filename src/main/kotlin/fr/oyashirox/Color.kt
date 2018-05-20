5584f4b6159c707a0d6dbd6c135ab5108538d232
Oyashirox <oyashirox@gmail.com>
2018-05-20 21:19:40 +0200

    Add image saving to file

---
+++ src/main/kotlin/fr/oyashirox/Color.kt
@@ -0,0 +1,17 @@
+package fr.oyashirox
+
+@Suppress("unused", "MemberVisibilityCanBePrivate", "NOTHING_TO_INLINE")
+data class Color(val r: Int, val g: Int, val b: Int, val a: Int = 0xFF) {
+    constructor(r: Byte, g: Byte, b: Byte, a: Byte = 255.toByte()) : this(r.toInt(), g.toInt(), b.toInt(), a.toInt())
+    constructor(r: Double, g: Double, b: Double, a: Double = 1.0) : this(
+            (r * 255).toInt(),
+            (g * 255).toInt(),
+            (b * 255).toInt(),
+            (a * 255).toInt()
+    )
+
+    var argbColor: Int = (a shl 24) or (r shl 16) or (g shl 8) or b
+
+    inline operator fun plus(o: Color) = Color(r + o.r, g + o.g, b + o.b)
+}
+
--- src/main/kotlin/fr/oyashirox/Main.kt
+++ src/main/kotlin/fr/oyashirox/Main.kt
@@ -1,5 +1,24 @@
 package fr.oyashirox
 
+import java.awt.Desktop
+import java.awt.image.BufferedImage
+import java.nio.file.Paths
+import javax.imageio.ImageIO
+
 fun main(args: Array<String>) {
-    println("Hello test")
+    val image = BufferedImage(1280, 720, BufferedImage.TYPE_INT_ARGB)
+
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val colorX = Color(x.toDouble()/image.width.toDouble(), 0.0, 0.0)
+            val colorY = Color(0.0, y.toDouble()/image.height.toDouble(), 0.0)
+            val combinedColor = colorX + colorY
+            image.setRGB(x, y, combinedColor.argbColor)
+        }
+    }
+
+    val file = Paths.get(".", "generated.png").toAbsolutePath().normalize().toFile()
+    println("Image saved to : ${file.absolutePath}")
+    ImageIO.write(image, "png", file)
+    Desktop.getDesktop().open(file)
 }
