e99145eba5e15a0587dab40a92e01d098b6994dd
Oyashirox <oyashirox@gmail.com>
2018-07-25 21:59:23 +0200

    Update Kotlin, removes untracked files

--- src/main/kotlin/fr/oyashirox/shape/HitableList.kt
+++ src/main/kotlin/fr/oyashirox/shape/HitableList.kt
@@ -6,10 +6,8 @@ import fr.oyashirox.raytracing.NoHit
 import fr.oyashirox.raytracing.Ray
 
 @Suppress("unused", "MemberVisibilityCanBePrivate")
-class HitableList(vararg items: Hitable) : Hitable {
-    val items: List<Hitable> = items.toList()
-
-    constructor(items: List<Hitable>): this(*(items.toTypedArray()))
+class HitableList(val items: List<Hitable>) : Hitable {
+    constructor(vararg items: Hitable) : this(items.toList())
 
     override fun hit(ray: Ray, min: Double, max: Double): Hit {
         var closest = max
@@ -17,7 +15,8 @@ class HitableList(vararg items: Hitable) : Hitable {
         for (hitable in items) {
             val hit = hitable.hit(ray, min, closest)
             when (hit) {
-                is HitData -> {
+                is HitData ->
+                    if (hit.distance < closest) {
                     closest = hit.distance
                     result = hit
                 }
